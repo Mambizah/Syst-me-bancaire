c667d770efa1d914f1d3bf1c7e5bf2279089932f
MKfery <ferytail360@gmail.com>
2026-04-17 10:07:14 +0000

    Add home redirect to login

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+def home(request):
+    return HttpResponseRedirect(reverse('login'))
